f53922f356cd95e6f7744d36ed08d99e1eb9c85d
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-07-02 12:37:13 +0600

    displaying posts with template tags

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 def posts_list(request):   #it going to receive request
-    return render(request, "posts/posts_list.html")
+    posts = Post.objects.all().order_by('-date')
+    return render(request, "posts/posts_list.html", {'posts': posts})
