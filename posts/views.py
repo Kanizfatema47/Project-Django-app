9c24161328e21aa5acf1a849e481797c345e811f
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-07-02 14:16:47 +0600

    creating a post page view func and passing a parameter with named url

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 def posts_list(request):   #it going to receive request
     posts = Post.objects.all().order_by('-date')
     return render(request, "posts/posts_list.html", {'posts': posts})
+
+
+#new func for post_page
+
+def post_page(request,slug):
+    return HttpResponse(slug)
