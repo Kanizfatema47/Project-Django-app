cd52fb161d4460c87f53d8432b1847ef95770778
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-07-02 14:26:28 +0600

    updating the post page view func and creating a post page template

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import HttpResponse
 def posts_list(request):   #it going to receive request
     posts = Post.objects.all().order_by('-date')
     return render(request, "posts/posts_list.html", {'posts': posts})
@@ -9,4 +8,5 @@ def posts_list(request):   #it going to receive request
 #new func for post_page
 
 def post_page(request,slug):
-    return HttpResponse(slug)
+    post = Post.objects.get(slug=slug)
+    return render (request, 'posts/post_page.html',{'post': post})
