50cf01abe43701669ae8f833f63b54811fc2d837
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-07-03 13:56:00 +0600

    adding new post page and applying user authenication

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
+
+
 def posts_list(request):   #it going to receive request
     posts = Post.objects.all().order_by('-date')
     return render(request, "posts/posts_list.html", {'posts': posts})
@@ -10,3 +13,7 @@ def posts_list(request):   #it going to receive request
 def post_page(request,slug):
     post = Post.objects.get(slug=slug)
     return render (request, 'posts/post_page.html',{'post': post})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
